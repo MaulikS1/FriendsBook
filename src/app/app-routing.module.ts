5056828812e79efac3595700d868192b89e958cb
MaulikS1 <mauliksolanki48@gmail.com>
2022-11-24 15:43:29 +0530

    added header style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 
 const routes: Routes = [
   { path: 'registration', component: RegistrationComponent },
+  { path: 'forgotPassword', component: ForgotPasswordComponent },
   { path: 'login', component: LoginComponent },
   { path: 'about', component:  AboutComponent},
   { path: '', component: WelcomeComponent },
