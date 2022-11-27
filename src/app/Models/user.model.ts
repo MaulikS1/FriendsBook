4fddf5b3e68af037d3158a37e94ba1ce0eba702c
MaulikS1 <mauliksolanki48@gmail.com>
2022-11-27 14:01:07 +0530

    Login Api changes

--- src/app/Models/user.model.ts
+++ src/app/Models/user.model.ts
@@ -1,28 +1,4 @@
-
 export interface IUserData {
-    id: number;
-    fname: string;
-    lname: string;
-    phone: string;
-    address: {
-      houseNumber: '';
-      city: '';
-      state: '';
-      landmark: '';
-    };
-    payment: {
-      name: '';
-      cardNumber: '';
-      month: '';
-      year: '';
-      cvv: '';
-    };
-    username: string;
-    password: string;
-    confirmpassword: string;
-    email: string;
-    orders: number[];
-    totalCost: number;
-    instantOrder: number;
-  }
-  
+  email: string;
+  password: string;
+}
